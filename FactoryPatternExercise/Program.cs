2e18704eab1ed36b3053a26b4004b27741cfc5c7
Tim Honchel <tim.honchel@gmail.com>
2021-12-21 10:45:14 -0500

    first commit

---
+++ FactoryPatternExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FactoryPatternExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many tires will your vehicle have?");
+            Console.WriteLine();
+            string tires = Console.ReadLine();
+            IVehicle vehicle = VehicleFactory.GetVehicle(tires);
+            vehicle.Drive();
+        }
+    }
+}
---
+++ FactoryPatternExercise/VehicleFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string tires)
+        {
+            switch (tires)
+            {
+                case "1":
+                case "one":
+                    return new Unicycle();
+                case "2":
+                case "two":
+                    return new Motorcycle();
+                case "3":
+                case "three":
+                    return new Tricycle();
+                case "4":
+                case "four":
+                    return new Car();
+                default:
+                    return new UFO();
+
+
+            }
+
+        }
+    }
+}
